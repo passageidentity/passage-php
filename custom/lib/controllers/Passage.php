7cf14b78ecba6730001a26b5aa95e703d85255a0
vanessa-passage <138504893+vanessa-passage@users.noreply.github.com>
2023-10-20 13:14:19 -0500

    Psg 2821 be write custom jwt token functions (#4)
    * JWT Token vallidation
    
    ---------
    
    Co-authored-by: Vanessa Burroughs <vanessa.burroughs@passage.id>

---
+++ custom/lib/controllers/Passage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Passage\Client\Controllers;
+
+class Passage {
+    private string $appId;
+    private string $apiKey;
+
+    /**
+     * Initialize a new Passage instance.
+     * @param appId $appId Passage app id (required)
+     * @param apiKey $apiKey Passage api key (required)
+     * 
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(string $appId, string $apiKey) {
+        $this->appId = $appId;
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * Get AppId key for this Passage instance
+     * @return string|null Passage API Key
+     */
+    public function getAppId(): string {
+        return $this->appId;
+    }
+}
+?>
---
+++ custom/test/AuthenticationTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use Firebase\JWT\JWT;
+use Passage\Client\Controllers\Authentication;
+use Passage\Client\Controllers\Passage;
+
+
+
+class AuthenticationTest extends TestCase {
+    private $appId;
+    private $apiKey;
+    private $appToken;
+    private $userId;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $config = include('config.php');
+
+        $this->appId = $config['APP_ID'];
+        $this->apiKey = $config['API_KEY'];
+        $this->appToken = $config['EXAMPLE_AUTH_TOKEN'];
+        $this->userId = $config['EXAMPLE_USER_ID'];
+    }
+
+    public function testValidJWT() {
+        $passage = new Passage($this->appId, $this->apiKey);
+        $authentication = new Authentication($passage);
+        
+        $user = $authentication->validateJWT($this->appToken);
+        $this->assertEquals($this->userId, $user);
+    }
+
+    public function testInvalidJWT() {
+        $passage = new Passage($this->appId, $this->apiKey);
+        $authentication = new Authentication($passage);
+        
+        $user = $authentication->validateJWT('incorrect.token');
+        $this->assertEquals(null, $user);
+    }
+}
