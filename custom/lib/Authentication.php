9b527e41b761f519f278f8313e8979fb6e54beef
Amy Gebhardt (she / her) <amy.gebhardt@agilebits.com>
2024-12-03 09:26:29 -0600

    chore: resolve linting errors and warnings per PSR-12 (#83)
    * chore: add phpcs
    
    * chore: auto fix linting errors with phpcbf
    
    * chore: fixed the majority of linting errors
    
    * chore: updating the git workflow to use the correct linting standard
    
    * chore: fixing the namespace error
    
    * chore: fixing the formatter

--- custom/lib/Authentication.php
+++ custom/lib/Authentication.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Passage\Client;
 
 use GuzzleHttp\Client;
@@ -7,37 +8,39 @@ use GuzzleHttp\Psr7\Request;
 use Firebase\JWT\JWT;
 use Firebase\JWT\CachedKeySet;
 use Phpfastcache\CacheManager;
-
 use OpenAPI\Client\ApiException;
 
-require 'vendor/autoload.php';
-
-class Authentication {
+class Authentication
+{
     private $passage;
     private $jwks;
 
     /**
      * Initialize a new Passage instance.
+     *
      * @param Passage $passage
-     * 
+     *
      * @throws \InvalidArgumentException
      */
-    public function __construct(Passage $passage) {
+    public function __construct(Passage $passage)
+    {
         $this->passage = $passage;
 
         $appId = $this->passage->getAppId();
         $jwtIssuer = 'https://auth.passage.id/v1/apps/' . $appId . '/.well-known/jwks.json';
-        
+
         $this->jwks = $this->fetchJWKS($jwtIssuer);
     }
-    
+
     /**
      * Returns the JWKS for the current app
      *
      * @param string $url
+     *
      * @return CachedKeySet UserId of the Passage user
-    */
-    private function fetchJWKS(string $url): CachedKeySet {
+     */
+    private function fetchJWKS(string $url): CachedKeySet
+    {
         $httpClient = new Client();
         $httpFactory = new HttpFactory();
 
@@ -59,10 +62,12 @@ class Authentication {
      * Determine if the provided token is valid when compared with its
      * respective public key.
      *
-     * @param string Authentication token
+     * @param string $jwtString Authentication token
+     *
      * @return string sub claim if the jwt can be verified, or Error
-    */
-    public function validateJWT(string $jwtString): string | null {
+     */
+    public function validateJWT(string $jwtString): string | null
+    {
         try {
             $decodedHeader = JWT::urlsafeB64Decode(explode('.', $jwtString)[0]);
             $header = json_decode($decodedHeader);
@@ -70,30 +75,28 @@ class Authentication {
             $kid = $header->kid;
 
             if (!$kid) {
-              throw new ApiException(
-                'Could not verify token: Missing kid in token',
-                401
-              );
+                throw new ApiException(
+                    'Could not verify token: Missing kid in token',
+                    401
+                );
             }
 
             $decodedToken = JWT::decode($jwtString, $this->jwks);
             $userID = $decodedToken->sub;
-      
+
             if ($userID) {
-              return strval($userID);
+                return strval($userID);
             } else {
-              throw new ApiException(
-                'Could not verify token: Could not retrieve user id',
-                401
-              );
+                throw new ApiException(
+                    'Could not verify token: Could not retrieve user id',
+                    401
+                );
             }
-          } catch (\Exception $e) {
+        } catch (\Exception $e) {
             throw new ApiException(
-              'Could not verify token: ' . $e->getMessage(),
-              401
+                'Could not verify token: ' . $e->getMessage(),
+                401
             );
         }
     }
 }
-
-?>
