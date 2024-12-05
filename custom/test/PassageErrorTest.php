7934f53e89dbcc8dadfe62fc3f8ba57f499fd38c
Chris Tran <chris.tran@agilebits.com>
2024-12-05 00:12:02 +0000

    tests: adds tests for auth, user, and error classes

--- custom/test/PassageErrorTest.php
+++ custom/test/PassageErrorTest.php
@@ -10,16 +10,19 @@ class PassageErrorTest extends TestCase
 {
     public function testFromApiException()
     {
-      $apiException = new ApiException(
-        code: 404,
-        message: "Not Found",
-        responseHeaders: [],
-        responseBody: '{"code":"user_not_found","error":"User not found"}'
-      );
-      $passageError = PassageError::fromApiException($apiException);
+        $apiException = new ApiException(
+            code: 404,
+            message: "Not Found",
+            responseHeaders: [],
+            responseBody: '{"code":"user_not_found","error":"User not found"}'
+        );
+        $passageError = PassageError::fromApiException($apiException);
 
-      $this->assertEquals(404, $passageError->getStatusCode());
-      $this->assertEquals("user_not_found", $passageError->getErrorCode());
-      $this->assertEquals("Passage\Client\PassageError: [404 user_not_found]: User not found\n", $passageError->getMessage());
+        $this->assertEquals(404, $passageError->getStatusCode());
+        $this->assertEquals("user_not_found", $passageError->getErrorCode());
+        $this->assertEquals(
+            "Passage\Client\PassageError: [404 user_not_found]: User not found\n",
+            $passageError->getMessage(),
+        );
     }
 }
