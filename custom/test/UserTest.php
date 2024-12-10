4836636294f7b67ba6aa4871380f6bf376618588
Chris Tran <chris.tran@agilebits.com>
2024-12-10 21:12:33 +0000

    feat: add new classes for PassageUser, CreateUserArgs, and UpdateUserArgs

--- custom/test/UserTest.php
+++ custom/test/UserTest.php
@@ -3,6 +3,9 @@
 namespace Passage\Test;
 
 use Dotenv\Dotenv;
+use OpenAPI\Client\Model\UpdateUserRequest;
+use Passage\Client\CreateUserArgs;
+use Passage\Client\UpdateUserArgs;
 use PHPUnit\Framework\TestCase;
 use Passage\Client\Passage;
 use Passage\Client\PassageError;
@@ -11,6 +14,7 @@ class UserTest extends TestCase
 {
     private $appId;
     private $apiKey;
+    private $userId;
     private $passage;
 
     public function setUp(): void
@@ -22,6 +26,7 @@ class UserTest extends TestCase
 
         $this->appId = getenv('APP_ID');
         $this->apiKey = getenv('API_KEY');
+        $this->userId = getenv('EXAMPLE_USER_ID');
 
         $this->passage = new Passage($this->appId, $this->apiKey);
     }
@@ -30,5 +35,29 @@ class UserTest extends TestCase
     {
         $this->expectException(PassageError::class);
         $this->passage->user->getByIdentifier('error@passage.id');
+    }
+
+    public function testCreate()
+    {
+        $expectedEmail = 'php@test.com';
+        $args = new CreateUserArgs();
+        $args->setEmail($expectedEmail);
+
+        $actual = $this->passage->user->create($args);
+
+        $this->assertEquals($expectedEmail, $actual->getEmail());
+
+        $this->passage->user->delete($actual->getId());
+    }
+
+    public function testUpdate()
+    {
+        $expectedPhone = '+15125555555';
+        $options = new UpdateUserArgs();
+        $options->setPhone($expectedPhone);
+
+        $actual = $this->passage->user->update($this->userId, $options);
+
+        $this->assertEquals($expectedPhone, $actual->getPhone());
     }
 }
