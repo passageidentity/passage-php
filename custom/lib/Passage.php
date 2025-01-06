90f8fe4a9fdeb78e8f0af5ff5717622f5ab8eed0
Chris Tran <chris.tran@agilebits.com>
2025-01-06 17:20:17 -0600

    feat: adds parameter guards to the Passage constructor

--- custom/lib/Passage.php
+++ custom/lib/Passage.php
@@ -2,6 +2,7 @@
 
 namespace Passage\Client;
 
+use InvalidArgumentException;
 use OpenAPI\Client\Configuration;
 
 class Passage
@@ -17,6 +18,18 @@ class Passage
      */
     public function __construct(string $appId, string $apiKey)
     {
+        if (!$appId) {
+            throw new InvalidArgumentException(
+                'A Passage App ID is required. Please include (YOUR_APP_ID, YOUR_API_KEY).'
+            );
+        }
+
+        if (!$apiKey) {
+            throw new InvalidArgumentException(
+                'A Passage API key is required. Please include (YOUR_APP_ID, YOUR_API_KEY).'
+            );
+        }
+
         $clientConfig = new Configuration();
         $clientConfig->setAccessToken($apiKey);
 
