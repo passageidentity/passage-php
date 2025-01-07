a2d8209f148f5594bb880f88fcbb48e7f7cc5e15
Chris Tran <ctran88@users.noreply.github.com>
2025-01-07 11:35:08 -0600

    feat: adds parameter guards to the Passage constructor (#101)

--- custom/test/PassageTest.php
+++ custom/test/PassageTest.php
@@ -4,18 +4,9 @@ namespace Passage\Test;
 
 use PHPUnit\Framework\TestCase;
 use OpenAPI\Client\HeaderSelector;
-use Passage\Client\Passage;
 
 class PassageTest extends TestCase
 {
-    public function testConstructorMissingParam()
-    {
-        $this->expectException(\ArgumentCountError::class);
-        $this->expectExceptionMessage('Too few arguments to function Passage\Client\Passage::__construct()');
-
-        new Passage('123456');
-    }
-
     public function testPassageVersionHeader()
     {
         $headerSelector = new HeaderSelector();
