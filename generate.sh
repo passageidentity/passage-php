b041c47a5f17a3c3ff29741646cb65537b2550f6
Chris Tran <ctran88@users.noreply.github.com>
2025-01-10 13:17:00 -0600

    build: change codegen to move generated files to src/generated (#103)

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ fi
 
 file="$1"
 
-rm -rf ./generated
+rm -rf ./src/generated/*
 
 docker run --rm -v "${PWD}:/local" -u $(id -u) openapitools/openapi-generator-cli:latest generate \
   -i "/local/$file" \
@@ -16,14 +16,13 @@ docker run --rm -v "${PWD}:/local" -u $(id -u) openapitools/openapi-generator-cl
   -o /local/temp \
   --global-property apiTests=false,modelTests=false,apiDocs=false,modelDocs=false
 
-mkdir -p ./generated/lib
-mv ./temp/src/* ./generated/lib
+mv ./temp/src/* ./src/generated
 rm -rf ./temp
 
 add_passage_version_header() {
   local header_comment="// Add Passage version header"
 
-  if grep -q "$header_comment" generated/lib/HeaderSelector.php; then
+  if grep -q "$header_comment" src/generated/HeaderSelector.php; then
     echo "Passage version header already exists"
     return 0
   fi
@@ -42,18 +41,18 @@ add_passage_version_header() {
 \\
         \$headers['Passage-Version'] = '$package_name ' . \$packageVersion;\\
 " \
-  generated/lib/HeaderSelector.php
+  src/generated/HeaderSelector.php
 }
 
 # php-nextgen is generating error model return values from the API instead of void for some operations
 # so this changes the return value back to void
 return_void() {
   local bad_user_return_type=' \\OpenAPI\\Client\\Model\\Model401Error|\\OpenAPI\\Client\\Model\\Model404Error|\\OpenAPI\\Client\\Model\\Model500Error'
-  sed -i 's/'"$bad_user_return_type"'/ void/' generated/lib/Api/UsersApi.php
-  sed -i 's/'"$bad_user_return_type"'/ void/' generated/lib/Api/UserDevicesApi.php
+  sed -i 's/'"$bad_user_return_type"'/ void/' src/generated/Api/UsersApi.php
+  sed -i 's/'"$bad_user_return_type"'/ void/' src/generated/Api/UserDevicesApi.php
 
   local bad_token_return_type=' \\OpenAPI\\Client\\Model\\Model401Error|\\OpenAPI\\Client\\Model\\Model403Error|\\OpenAPI\\Client\\Model\\Model404Error|\\OpenAPI\\Client\\Model\\Model500Error'
-  sed -i 's/'"$bad_token_return_type"'/ void/' generated/lib/Api/TokensApi.php
+  sed -i 's/'"$bad_token_return_type"'/ void/' src/generated/Api/TokensApi.php
 }
 
 add_passage_version_header
